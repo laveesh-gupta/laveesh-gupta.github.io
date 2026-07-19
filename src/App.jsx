83efc7acb66d9a7e7582390548cf8368381c3950
LG5 <lg5@localhost>
2026-07-19 19:11:43 +0530

    Favicon change

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,13 @@ import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
 function App() {
+  console.log(
+    "%cGreetings!",
+    "color: #00ff00; font-size: 16px; font-weight: bold;",
+  );
+  console.log(
+    "Since you're digging around in the console, you might be the kind of developer I want to work with. Let's chat!",
+  );
   return (
     <main className="app">
       <Hero />
